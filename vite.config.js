bea446a23452356868697ee752de7d7702faddf6
Moataz <mtzadl00@gmail.com>
2025-12-10 17:15:58 +0300

    Add vite config and fix Render build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
+  },
+});
